276e869d49c9d59059b83140a34fb2ea393dbdf5
Jose <aleangulo@arminsa.com>
2021-10-24 15:10:39 -0600

    Actualizacion de aplicaciones python

--- Comunication/Client.py
+++ Comunication/Client.py
@@ -1,11 +1,14 @@
 import socket
+import time
 
-HOST = '192.168.100.63'  # The server's hostname or IP address
+HOST = '192.168.100.26'  # The server's hostname or IP address
 PORT = 65432        # The port used by the server
-
+#prueba
+a = "Feliz"
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-    s.sendall(b'hello World')
-    data = s.recv(1024)
-
-print('Received', repr(data))
+    while True:
+        s.sendall(a.encode())
+        data = s.recv(1024)
+        print('Received', repr(data))
+        time.sleep(2)
