f23bb6ebd07c9cc8869d046c72f11ce544b4ddf5
Jose Angulo <joseangulo@MacBook-Pro-de-Jose.local>
2021-10-25 01:32:20 -0600

    Aplicacion sin skimage

--- Python/main_lite.py
+++ Python/main_lite.py
@@ -1,36 +1,34 @@
 import cv2
-#import tensorflow as tf
+# import tensorflow as tf
 import tflite_runtime.interpreter as tflite
 import numpy as np
-import socket
-from skimage.transform import resize
 
-import socket
 
-HOST = '192.168.100.26'  # The server's hostname or IP address
-PORT = 65432        # The port used by the server
+def crop_center(img, x, y, w, h):
+    return img[y:y + h, x:x + w]
 
-def crop_center(img, x, y, w, h):    
-    return img[y:y+h,x:x+w]
 
 def preprocess_img(raw):
-    img = resize(raw,(200,200, 3))
-    #print(raw.shape)
-    #img = cv2.resize(raw,(200,200))
-    #img = raw.reshape(200,200,3)
-    img = np.expand_dims(img,axis=0)
-    if(np.max(img)>1):
-        img = img/255.0
+    print(raw.shape)
+    # img = resize(raw,(200,200, 3))
+    # print(raw.shape)
+    img = cv2.resize(raw, (200, 200))
+    # img = raw.reshape(200,200,3)
+    img = np.expand_dims(img, axis=0)
+    if (np.max(img) > 1):
+        img = img / 255.0
+    print(img.shape)
     return img
+
 
 def brain(raw, x, y, w, h):
     ano = ''
-    img = crop_center(raw, x, y , w , h)
+    img = crop_center(raw, x, y, w, h)
     img = preprocess_img(img)
     f.set_tensor(i['index'], img.astype(np.float32))
     f.invoke()
     res = f.get_tensor(o['index'])
-    classes = np.argmax(res,axis=1)
+    classes = np.argmax(res, axis=1)
     if classes == 0:
         ano = 'anger'
     elif classes == 1:
@@ -43,16 +41,16 @@ def brain(raw, x, y, w, h):
         ano = "neutral"
     elif classes == 5:
         ano = 'sadness'
-    else :
+    else:
         ano = 'surprised'
     return ano
-    
+
 
 print('Loading ..')
 
-#f = tf.lite.Interpreter("models/model_optimized.tflite")
-f = tflite.Interpreter("/usr/bin/model_optimized.tflite")
-#f = f = tflite.Interpreter("models/model_optimized.tflite")
+# f = tf.lite.Interpreter("models/model_optimized.tflite")
+#f = tflite.Interpreter("/usr/bin/model_optimized.tflite")
+f = tflite.Interpreter("models/model_optimized.tflite")
 f.allocate_tensors()
 i = f.get_input_details()[0]
 o = f.get_output_details()[0]
@@ -63,44 +61,38 @@ cascPath = "haarcascade_frontalface_default.xml"
 
 faceCascade = cv2.CascadeClassifier(cascPath)
 
-
 cap = cv2.VideoCapture(0)
 ai = 'anger'
 img = np.zeros((200, 200, 3))
 ct = 0
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    while(True):
-        # Capture frame-by-frame
-        ret, frame = cap.read()
-        ct+=1
-        # Our operations on the frame come here
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+while (True):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+    ct += 1
+    # Our operations on the frame come here
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-        # Detect faces in the image
-        faces = faceCascade.detectMultiScale(
-            gray,
-            scaleFactor=1.1,
-            minNeighbors=5,
-            minSize=(150, 150)
-            #flags = cv2.CV_HAAR_SCALE_IMAGE
-        )
-    
-        ano = ''    
-        for (x, y, w, h) in faces:
-            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
-            cv2.putText(frame, ai, (x, y-5), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0,0,255), 2, cv2.LINE_AA)
-            if ct > 3:
-                ai = brain(gray, x, y, w, h)
-                s.connect((HOST, PORT))
-                s.sendall(ai.encode())
-                data = s.recv(1024)
-                print('Received', repr(data))
-                ct = 0
+    # Detect faces in the image
+    faces = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(150, 150)
+        # flags = cv2.CV_HAAR_SCALE_IMAGE
+    )
 
-        # Display the resulting frame
-        cv2.imshow('frame',frame)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
+    ano = ''
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        cv2.putText(frame, ai, (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2, cv2.LINE_AA)
+        if ct > 3:
+            ai = brain(frame, x, y, w, h)
+            ct = 0
+
+    # Display the resulting frame
+    cv2.imshow('frame', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
 
 # When everything done, release the capture
 cap.release()
