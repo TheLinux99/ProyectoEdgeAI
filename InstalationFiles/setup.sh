f92c026ef3fdcb013b7fdcf52132d4646a186fcd
Jose Angulo <joseangulo@MacBook-Pro-de-Jose.local>
2021-10-25 01:36:21 -0600

    update de setup

--- InstalationFiles/setup.sh
+++ InstalationFiles/setup.sh
@@ -1,16 +1,14 @@
-pip3 --no-cache-dir install matplotlib
+mkdir ../EdgeAI
 
-pip3 --no-cache-dir install scikit-image
-
-mkdir EdgeAI
-
-cd EdgeAI
+cd ../EdgeAI
 
 git clone https://github.com/TheLinux99/ProyectoEdgeAI.git
 
 cd ProyectoEdgeAI
 
 mv Python ../
+
+cd ..
 
 rmdir ProyectoEdgeAI
 
